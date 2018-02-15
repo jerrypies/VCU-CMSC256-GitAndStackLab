75b9490a8c282bce6af1b04871f0acf9e95aaafb
Jeremy M Nguyen <nguyendm7@egr-maclab-10.local>
2018-02-15 15:00:09 -0500

    added array based stack

--- ArrayBasedStack.java
+++ ArrayBasedStack.java
@@ -1,3 +1,5 @@
+import java.util.Arrays; 
+
 public class ArrayBasedStack <T> implements StackInterface<T> {
 
 T[] data;
@@ -20,14 +22,14 @@ public ArrayBasedStack(int topOfStack){
    if(topOfStack < 0) {
       throw new IllegalArgumentException("Must be greater than zero"); 
       }
-}
+ }
 
 public ArrayBasedStack(){
    data = (T[]) new Object[INITIAL_CAPACITY];
     topOfStack = 0;
     initialized = true; 
     
-}
+ }
 
 
 /**
@@ -39,29 +41,62 @@ public ArrayBasedStack(){
    /** Adds a new entry to the top of this stack.
        @param newEntry  An object to be added to the stack. */
    public void push(T newEntry){
+      if(topOfStack > data.length){
+      
+      }
+      else{topOfStack = topOfStack + 1; }
+      
+      
+      data[topOfStack] = newEntry;
+      
    }
+   
+   
   
    /** Removes and returns this stack's top entry.
        @return  The object at the top of the stack. 
        @throws  EmptyStackException if the stack is empty before the operation. */
    public T pop(){
+    if(isEmpty()){
+      throw new IllegalArgumentException("Empty.");
+      }
+       
+   return data[topOfStack];
+      
    }
   
    /** Retrieves this stack's top entry.
        @return  The object at the top of the stack.
        @throws  EmptyStackException if the stack is empty. */
    public T peek(){
+    if(isEmpty()) {
+      throw new IllegalArgumentException("Empty."); 
+      }
+      return data[topOfStack];
+     
    }
   
    /** Detects whether this stack is empty.
        @return  True if the stack is empty. */
    public boolean isEmpty(){
-   return true;
+      if(topOfStack < 0){
+      return true;
+      }
+      else{
+      return false;
+      }
    }
   
    /** Removes all entries from this stack. */
    public void clear(){
+   isEmpty();
+   pop();
    }
    
+   private void expandArray(){
+   T[] copy = Arrays.copyOf(data,5);
+   }
+   
+ }
+   
 
-}
